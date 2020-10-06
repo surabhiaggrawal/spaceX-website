97630a8ae464113fe2970dc0427895beba358751
Surabhi Aggrawal <surabhi.aggrawal@harley-davidson.com>
2020-10-06 17:07:35 +0530

    Filter changes

--- js/app.js
+++ js/app.js
@@ -1,8 +1,34 @@
+var gbldata;
+const state = { year:'', land:'', launch:'' };
 
 window.onload = function(){
+    state.year = '';
+    state.land = '';
+    state.launch = '';
+    
     loadCardDetails(false);
+    
+    if(sessionStorage.getItem('localUrl') == null || sessionStorage.getItem('localUrl') == undefined){
+        sessionStorage.setItem('localUrl',window.location.href);
+        window.history.pushState(state, '', window.location.href);
+    } 
 }
 
+window.onpopstate = (event) => {
+    console.log(event.state);
+
+    if(event.state) {
+        if(event.state.year || event.state.land || event.state.launch ){
+            state.year = event.state.year;
+            state.land = event.state.land;
+            state.launch = event.state.launch;
+            checkFilterCombination(false);
+        } 
+    }else {
+            loadCardDetails(true);
+    }
+    
+};
 
 var loadCardDetails = function(reload) {
     $('.lds-spinner').css('display','inline-block');
@@ -14,6 +40,7 @@ var loadCardDetails = function(reload) {
         dataType: "json",
         success: function (data) {
             if (data != null || data != undefined) {
+                gbldata = data;
                 renderCardDetails(data,reload);
                 trackRadioSelection();                
             } else {
@@ -31,6 +58,7 @@ var loadCardDetails = function(reload) {
 
 
 var renderCardDetails = function (data,renderCount) {
+    console.log(data);
 
     let htm;
     if(renderCount){
@@ -48,6 +76,14 @@ var renderCardDetails = function (data,renderCount) {
         htm = $('.cards-container').html(noResultsDiv);
 
     } else{
+
+        let landCheck;
+        if($('input[type="radio"][name=success-land-btn]:checked').length){
+            landCheck = $('input[type="radio"][name=success-land-btn]:checked').val().toLowerCase();
+        } else{
+            landCheck = false;
+        }
+        
         data.forEach(element => {
             //making list of mission Id's and storing in list variable
             let list = '';
@@ -94,7 +130,7 @@ var renderCardDetails = function (data,renderCount) {
                 </div>
                 <div class="success-landing-card card-sec">
                     <span class="mission-label">Successful Landings:</span>
-                    <span class="mission-details">${element.is_tentative}</span>
+                    <span class="mission-details">`+ landCheck +`</span>
                 </div>
             </div>
             </div>`
@@ -104,13 +140,11 @@ var renderCardDetails = function (data,renderCount) {
         $('.lds-spinner').css('display','none');
         $('.cards-container').css('display','flex');
         $('.cards-container').html(htm);
-    }
-
-    
+    }  
 }
 
 var collectFilterData = function() {
-    let launchYr = 0;
+    let launchYr = undefined;
     let successfulLaunch = undefined;
     let successfulLand = undefined;
 
@@ -127,25 +161,82 @@ var collectFilterData = function() {
         console.log('yes selected' + successfulLand);
     } 
     
-    callApi(launchYr,successfulLaunch,successfulLand);
+    state.land = successfulLand;
+    state.launch = successfulLaunch;
+    state.year = launchYr;
+
+    checkFilterCombination();
 }
 
-var callApi = function(year,launch,land){
+var checkFilterCombination = function(setState = true){
 
     let dynamicUrl;
+    let dynamicWindowUrl;
+    let land = state.land ;
+    let launch = state.launch;
+    let year = state.year;
+    let callApiToken = false;
+    let checkFilterscomb = '';
 
     $('.lds-spinner').css('display','inline-block');
     $('.cards-container').css('display','none');
 
     if(year && launch && land) {
-        dynamicUrl = 'https://api.spacexdata.com/v3/launches?limit=100&launch_success='+launch+'&land_success='+land+'&launch_year='+year; 
-    }else if(launch && land) {
-        dynamicUrl = 'https://api.spaceXdata.com/v3/launches?limit=100&launch_success='+launch+'&land_success='+land;
-    }else if(launch) {
-        dynamicUrl = 'https://api.spaceXdata.com/v3/launches?limit=100&launch_success='+launch;
+        dynamicUrl = 'https://api.spacexdata.com/v3/launches?limit=100&launch_success='+launch+'&land_success='+land+'&launch_year='+year;
+        callApiToken = true;
     }
+    else if(launch && land) {
+        dynamicUrl = 'https://api.spaceXdata.com/v3/launches?limit=100&launch_success='+launch+'&land_success='+land;
+        callApiToken = true;
+    } 
+    else if(year && land){
+        dynamicUrl = '%&land_success='+land+'&launch_year='+ year;
+        callApiToken = false;
+        checkFilterscomb = 'yland';
+    } 
+    else if(year && launch){
+        dynamicUrl = '%&launch_success='+launch+'&launch_year='+ year;
+        callApiToken = false;
+        checkFilterscomb = 'ylaunch';
+    } 
+    else if(launch) {
+        dynamicUrl = 'https://api.spaceXdata.com/v3/launches?limit=100&launch_success='+launch;
+        callApiToken = true;
+    } 
+    else if(year){
+        dynamicUrl = '%&launch_year='+ year;
+        callApiToken = false;
+        checkFilterscomb = 'yr';
+    } 
+    else if(land) {
+        dynamicUrl = '%&land_success='+land;
+        callApiToken = false;
+        checkFilterscomb = 'land';
+    }
+    
+        //Creating urls for tab propagation
+        if(dynamicUrl.indexOf('&') != 0){
+            let slicedUrl = dynamicUrl.slice(dynamicUrl.indexOf('&'));
+            dynamicWindowUrl = sessionStorage.getItem('localUrl') + '%20' + slicedUrl;
+        } else {
+            dynamicWindowUrl = sessionStorage.getItem('localUrl');
+        }
 
-    if(dynamicUrl !== undefined){
+        if(setState){
+            window.history.pushState(state, '', dynamicWindowUrl);
+        }
+        
+        callApiForSelectedFilter(dynamicUrl,callApiToken,checkFilterscomb);
+}
+
+
+var callApiForSelectedFilter = function(dynamicUrl,callApiToken,checkFilterscomb){
+
+    let land = state.land ;
+    let launch = state.launch;
+    let year = state.year;
+
+    if(callApiToken === true){
         $.ajax({
             type: "GET",
             url: dynamicUrl,
@@ -167,13 +258,76 @@ var callApi = function(year,launch,land){
             }
         });
     } else{
+
+        if (gbldata != null || gbldata != undefined) {
+            data = gbldata;
+            let filteredResult = [];
+            let filterOnYear = [];
+            
+            if(checkFilterscomb == 'yland'){
+                filterOnYear = data.filter(el => el.launch_year === year);
+                if(land === 'true'){
+                    filteredResult = filterOnYear.filter(checkForTrueFilter);
+                } else {
+                    filteredResult = filterOnYear.filter(checkForFalseFilter);
+                }
+            }
+
+            else if(checkFilterscomb == 'ylaunch'){
+                filterOnYear = data.filter(el => (el.launch_year).toString() === year);
+                filteredResult = filterOnYear.filter(el => (el.launch_success).toString() === launch);
+            }
+
+            else if(checkFilterscomb == 'yr'){
+                filteredResult = data.filter(el => (el.launch_year).toString() === year);
+            } 
+            
+            else if(checkFilterscomb == 'land'){
+                if(land === 'true'){
+                    filteredResult = data.filter(checkForTrueFilter);
+                } else {
+                    filteredResult = data.filter(checkForFalseFilter);
+                }
+            }
+
+            console.log('filteredResult',filteredResult);
+            renderCardDetails(filteredResult,true);
+        }
+
+    }
         $('.lds-spinner').css('display','none');
         $('.cards-container').css('display','flex');
+}
+
+    
+
+var checkForTrueFilter = function(el){
+    let checkedAll = false;
+       
+    if(el.rocket.first_stage.cores.length){
+        checkedAll = el.rocket.first_stage.cores.every((elem) => elem.land_success === true);
+    }
+    
+    if(checkedAll){
+        return true;
+    }
+}
+
+var checkForFalseFilter = function(el){
+    let checkedAll = false;
+       
+    if(el.rocket.first_stage.cores.length){
+        checkedAll = el.rocket.first_stage.cores.every((elem) => elem.land_success === false);
+    }
+    
+    if(checkedAll){
+        return true;
     }
 }
 
 var trackRadioSelection = function(){
     
+    //Disable land filters if Launch is false
     $('input[name=success-launch-btn][value=False]').click(function(){
         $('input[type="radio"][name=success-land-btn]').attr('disabled',true);
         $('input[name=success-land-btn]').prop("checked", false);
@@ -181,22 +335,26 @@ var trackRadioSelection = function(){
 
     });
 
+    //Unable land filters if Launch is false
     $('input[name=success-launch-btn][value=True]').click(function(){
         $('input[type="radio"][name=success-land-btn]').attr('disabled',false);
         $('label[for=True-land],label[for=False-land]').css("opacity", 1);
     });
 
+    //On click of any button check for other filters
     $('input[type="radio"][name=launch-yr-btn],input[type="radio"][name=success-launch-btn],input[type="radio"][name=success-land-btn]').click(function(){
        betterFunction();
       });
 
+    //Clear All button
       $('.spaceX-button').click(function(){
         $('input[name=success-launch-btn]').prop("checked", false); 
         $('input[name=launch-yr-btn]').prop("checked", false); 
         $('input[name=success-land-btn]').prop("checked", false); 
         $('label[for=True-land],label[for=False-land]').css("opacity", 1);
         $('input[type="radio"][name=success-land-btn]').attr('disabled',false);
-        loadCardDetails(true);
+        window.location.href = sessionStorage.getItem('localUrl');
+       // loadCardDetails(true);
       });
 }
 
@@ -214,4 +372,4 @@ const delayedSearch = function (fn, delay) {
         }, delay)
     }
 }
-const betterFunction = delayedSearch(collectFilterData, 1000);
+const betterFunction = delayedSearch(collectFilterData, 1500);
